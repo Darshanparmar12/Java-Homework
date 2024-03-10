a5632e79a59777724b07c3b0cf825b534655a154
Darshan Parmar <darshanparmar1535@gmail.com>
2024-03-10 23:56:04 +0000

    homework 3

---
+++ src/Area.java
@@ -0,0 +1,16 @@
+public class Area {
+
+        public static void main(String[] args) {
+            // Given radius and approximate value of pi
+            double radius = 10;
+            double pi = 3.14;
+
+            // Calculate the area of the circle
+            double area = pi * radius * radius;
+
+            // Print the result
+            System.out.println("The area of the circle with radius " + radius + " is: " + area);
+        }
+    }
+
+
