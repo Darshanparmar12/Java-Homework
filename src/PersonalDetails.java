b73fc596665d81ca39be7318bb826fd0d99be8d5
Darshan Parmar <darshanparmar1535@gmail.com>
2024-03-10 23:49:54 +0000

    homework 2

---
+++ src/PersonalDetails.java
@@ -0,0 +1,20 @@
+public class PersonalDetails {
+
+
+    public static String[] args;
+
+    public static void main(String[] args) {
+        String firstName = "Darshan";
+        String lastName = "Parmar";
+        String Country = "United Kingdom";
+        int age = 25;
+        int Number = 8151862;
+
+        System.out.println("First name " + firstName);
+        System.out.println("Last name " + lastName);
+        System.out.println("Country " + Country);
+        System.out.println("age " + age);
+        System.out.println("Mobile Number " + Number);
+
+    }
+}
